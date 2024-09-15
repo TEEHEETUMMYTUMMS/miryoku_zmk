a1c5a81b7371e66ff2323ea32f3d3a8b2883a6c8
TEEHEETUMMYTUMMS <112745959+TEEHEETUMMYTUMMS@users.noreply.github.com>
2024-09-15 02:47:36 -0400

    Add repeat u & h to mouse layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -40,8 +40,8 @@ U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_SYM, BSPC), U_LT
 #define MIRYOKU_LAYER_TAP \
 &kp SQT,           &kp COMMA,         &kp DOT,           &kp P,             &kp Y,             &kp F,             &kp G,             &kp C,             &kp R,             &kp L,             \
 &kp A,             &kp O,             &kp E,             &kp U,             &kp I,             &kp D,             &kp H,             &kp T,             &kp N,             &kp S,             \
-&kp SLASH,         &kp Q,             &kp J,             &kp K,             &kp X,             &kp B,             &kp M,             &kp W,             &kp V,             &kp Z,             \
-U_NP,              U_NP,              &kp DEL,           &kp BSPC,          U_LT(U_SYM, TAB),  &kp RET,           &kp SPACE,         &kp ESC,           U_NP,              U_NP
+&kp SEMI,          &kp Q,             &kp J,             &kp K,             &kp X,             &kp B,             &kp M,             &kp W,             &kp V,             &kp Z,             \
+U_NP,              U_NP,              &kp DEL,           &kp BSPC,          U_LT(U_NUM, TAB),  &kp RET,           &kp SPACE,         &kp ESC,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_BUTTON \
 &kp LC(Z),         U_CUT,             U_CPY,             U_PST,             &kp LC(Y),         &kp LC(Y),         U_PST,             U_CPY,             U_CUT,             &kp LC(Z),         \
@@ -61,7 +61,7 @@ U_NP,              U_NP,              &kp DEL,           &kp BSPC,          &kp 
 
 #define MIRYOKU_LAYER_MOUSE \
 U_WH_U,            U_WH_L,            U_MS_U,            U_WH_R,            U_NU,              &to U_BUTTON,      &to U_BASE,        &to U_EXTRA,       &to U_TAP,         U_BOOT,            \
-U_WH_D,            U_MS_L,            U_MS_D,            U_MS_R,            U_NU,              U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LGUI,          &kp LALT,          \
+U_WH_D,            U_MS_L,            U_MS_D,            U_MS_R,            &kp U,             &kp H,             &kp LSHFT,         &kp LCTRL,         &kp LGUI,          &kp LALT,          \
 &kp LC(Z),         U_CUT,             U_CPY,             U_PST,             &kp LC(Y),         U_NA,              &to U_MOUSE,       &to U_SYM,         &kp RALT,          U_NA,              \
 U_NP,              U_NP,              U_BTN3,            U_BTN2,            U_BTN1,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
